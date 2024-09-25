c2b779301b91a2dde7ae67b05ed729d73145b3b3
naaatami <144642305+naaatami@users.noreply.github.com>
2024-09-24 22:57:42 -0500

    updated error handling

--- main.cpp
+++ main.cpp
@@ -9,78 +9,72 @@
 using namespace std;
 
 double convert(double valueToConvert, Unit &initialUnit, Unit &endUnit);
-int findMapping(string initialUnit, string endUnit);
+bool findMapping(int &mapIndex, string initialUnit, string endUnit);
 
 int main(int argc, char *argv[]) {
-    if (argc == 3 || argc == 4)
-    {
+    // checking for correct amount of arguments entered
+    if (argc == 3 || argc == 4) {
         initializeUnitMaps();
 
         double initialValue = stod(argv[1]);
         string initialUnitString = argv[2];
         string endUnitString;
 
-        if(argv[3] == string("to") && argv[4])
-        {
+        if(argv[3] == string("to") && argv[4]) {
             endUnitString = argv[4];
-        } else {
+        }
+
+        else {
             endUnitString = argv[3];
         }
 
-        int mapIndex = findMapping(initialUnitString, endUnitString);
-        if(mapIndex == -1)
-        {
-            cout << "ERROR: The two units given cannot be converted between.";
+        // checks that units can be converted to and from before converting
+        int mapIndex = 0;
+        bool mappingFound = findMapping(mapIndex, initialUnitString, endUnitString);
+
+        if(!mappingFound) {
+            cout << "ERROR: The units provided cannot be converted between." << endl;
             return 0;
         }
 
         Unit initialUnit = unitList[mapIndex].at(initialUnitString);
         Unit endUnit = unitList[mapIndex].at(endUnitString);
-
         double endValue = convert(initialValue, initialUnit, endUnit);
 
+        // outputs final result
         cout << initialValue << " " << initialUnit.unitName << " = " << endValue << " " << endUnit.unitName << "." << endl;
-
         return 0;
+
+    // not enough arguments - provide correct usage
     } else {
-        cout << "ERROR: Incorrect format." << endl;
+        cout << "ERROR: Incorrect format" << endl;
+        cout << "Correct format: conv <value> <initial_unit> <end_unit>" << endl;
+        cout << "Example usage: conv 40 km m" << endl;
     }
 }
 
-double convert(double valueToConvert, Unit &initialUnit, Unit &endUnit){
+
+// converts from the initialUnit to endUnit
+double convert(double valueToConvert, Unit &initialUnit, Unit &endUnit) {
     double endValue = valueToConvert * initialUnit.conversionFactor;
     return endValue / endUnit.conversionFactor;
 }
 
-int findMapping(string initialUnit, string endUnit)
-{
+// finds the correct map index for two given units
+// returns false if nothing can be found
+bool findMapping(int &mapIndex, string initialUnit, string endUnit) {
     bool initialAndEndMatch = false;
-    for(int i = 0; i < unitList.size(); i++)
-    {
-        try
-        {
-            unitList[i].at(initialUnit);
+    while(mapIndex < unitList.size() && initialAndEndMatch == false) {
+        try {
+            unitList[mapIndex].at(initialUnit);
+            unitList[mapIndex].at(endUnit);
         }
-
-        catch(const std::out_of_range& ex)
-        {
-            continue;
+        catch(const std::out_of_range& ex) {
+            mapIndex++;
+            continue; // try to get rid of this
         }
-
-        try
-        {
-            unitList[i].at(endUnit);
-        }
-
-        catch(const std::out_of_range& ex)
-        {
-            continue;
-        }
-
         initialAndEndMatch = true;
-        return i;
     }
-    //better error handling needed here
-    return -1;
+    return initialAndEndMatch;
 }
 
